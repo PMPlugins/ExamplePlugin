b92112d51d5ed3f246ba3f99c227bfb70c7500b1
PhilipShilling <philipshilling@bootpanel.net>
2015-08-18 11:54:53 -0400

    JavaScript plugin update

--- JavaScript Plugin/ExamplePlugin.js
+++ JavaScript Plugin/ExamplePlugin.js
@@ -1,11 +1,19 @@
 /*
  * A JavaScript plugin is used to call API for plugins that allow it
  */
-
 function onEnable() {
-	PluginBase.getLogger().info("ExamplePlugin has been enabled!");
+	this.api.getServer().getCommandManager().registerCommand("example", "Just an example command", null, "example.cmd.example");
+	this.api.getLogger().info("ExamplePlugin has been enabled!");
+}
+
+function onCommand(sender, cmd, label, args) {
+	switch(cmd.getName()) {
+		case "example":
+			sender.sendMessage("This is an example command!");
+		break;
+	}
 }
 
 function onDisable() {
-	PluginBase.getLogger().warning("ExamplePlugin is no longer enabled! Did the server stop?");
+	this.api.getLogger().info("ExamplePlugin is no longer enabled! Dis the server stop?");
 }
